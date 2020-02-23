2b0f4c55009a8dd742df208679a766aeb0a6a1e7
Alexander Ivichev <Alexander.Ivichev@ru.ibm.com>
2020-02-23 14:02:19 +0300

    Add project files.

---
+++ Randomiser/Randomiser/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Randomiser
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Phone> Phones { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Phones = new List<Phone>();
+            this.BindingContext = this;
+        }
+
+        private void NewVariant_Clicked(object sender, EventArgs e)
+        {
+            //await DisplayAlert("New", "Added", "Ok");
+            Application.Current.MainPage = new NavigationPage(new NewVariant(this.Phones, this));
+        }
+        private async void Start_Clicked(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(0, this.Phones.Count);
+            var phone = this.Phones[value];
+            await DisplayAlert("New", phone.Name, "Ok");
+        }        
+    }
+
+    public class Phone
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Randomiser/Randomiser/NewVariant.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Randomiser
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewVariant : ContentPage
+	{
+        IList<Phone> phones;
+        MainPage mainPage;
+
+        public NewVariant (IList<Phone> phones, MainPage mainPage)
+		{
+			InitializeComponent ();
+            this.phones = phones;
+            this.mainPage = mainPage;
+		}
+
+        private void VariantAdded_Clicked(object sender, EventArgs e)
+        {
+            var newvariant = variant.Text;
+            this.phones.Add(new Phone() { Name = newvariant });
+            Application.Current.MainPage = new NavigationPage(mainPage);
+        }
+    }
+}
